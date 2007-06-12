a451332789b3fe9e74fddfa252d0226cf522aca5
secondlife <secondlife@77f01cae-a01f-0410-a9fa-f4a6a0bac188>
2007-06-12 10:35:45 +0000

    git-svn-id: http://hatenaapigraph.rubyforge.org/svn/trunk@7 77f01cae-a01f-0410-a9fa-f4a6a0bac188

--- lib/hatena/api/graph.rb
+++ lib/hatena/api/graph.rb
@@ -5,13 +5,18 @@ require 'sha1'
 require 'net/http'
 require 'uri'
 require 'time'
+require 'yaml'
 
 module Hatena
   module API
     class GraphError < StandardError; end
     class Graph
       DATE_FORMAT = '%Y-%m-%d'
-      GRAPH_API_URI = URI.parse 'http://graph.hatena.ne.jp/api/post'
+      GRAPH_API_URL = 'http://graph.hatena.ne.jp/api/'
+      GRAPH_API_DATA_URI = URI.parse GRAPH_API_URL + 'data'
+      GRAPH_API_CONFIG_URI = URI.parse GRAPH_API_URL + 'config'
+
+      CONFIG_BOOLEAN_KEYS = %w[showdata stack reverse formula nolabel]
 
       def initialize(username, password)
         @username = username
@@ -20,27 +25,105 @@ module Hatena
       end
       attr_accessor :proxy
 
-      def post(graphname, date, value)
-        value = value.to_f
-        date = date.strftime DATE_FORMAT
-        headers = {
-          'Access' => 'application/x.atom+xml, application/xml, text/xml, */*',
-          'X-WSSE' => wsse(@username, @password),
-        }
+      # graph.post 'graphname', :value => 10
+      # graph.post 'graphname', :value => 10, :date => Time.now
+      def post_data(graphname, options, value = nil)
+        if value.nil?
+          params = {
+            :graphname => graphname,
+          }.merge options
+        else
+          # obsolute arguments.
+          date = options
+          params = {
+            :graphname => graphname,
+            :date => date,
+            :value => value,
+          }
+        end
+
+        if params[:value] != nil
+          params[:value] = params[:value].to_f
+        end
+
+        params[:date] = params[:date].strftime(DATE_FORMAT) if params[:date]
+
+        res = http_post GRAPH_API_DATA_URI, params
+        raise GraphError.new("request not successed: #{res}") if res.code != '201'
+        res
+      end
+      alias_method :post, :post_data
+
+      def get_data(graphname, options = {})
         params = {
           :graphname => graphname,
-          :date => date,
-          :value => value,
+          :type => 'yaml',
+        }.merge options
+        res = http_get GRAPH_API_DATA_URI, params
+        p GRAPH_API_DATA_URI, params
+        raise GraphError.new("request not successed: #{res}") if res.code != '200'
+        YAML::load res.body
+      end
+
+      def get_config(graphname)
+        params = {
+          :graphname => graphname,
+          :type => 'yaml',
         }
-        res = http_post GRAPH_API_URI, params, headers
+        res = http_get GRAPH_API_CONFIG_URI, params
+        raise GraphError.new("request not successed: #{res}") if res.code != '200'
+        config = YAML::load res.body
+        config_booleanize config
+      end
+
+      def post_config(graphname, options)
+        params = {
+          :graphname => graphname,
+        }.merge options
+        params = boolean2queryparam(params)
+        res = http_post GRAPH_API_CONFIG_URI, params
         raise GraphError.new("request not successed: #{res}") if res.code != '201'
         res
       end
 
-      private 
-      def http_post(url, params, headers)
-        req = ::Net::HTTP::Post.new(url.path, headers)
-        req.form_data = params
+      private
+      def config_booleanize(config)
+        CONFIG_BOOLEAN_KEYS.each do |key|
+          config[key] = (config[key] == 1 ? true : false) if config.has_key? key
+        end
+        config
+      end
+
+      def boolean2queryparam(config)
+        CONFIG_BOOLEAN_KEYS.each do |key|
+          config[key] = (config[key] ? 1 : 0) if(config.has_key?(key) || config.has_key?(key.to_sym))
+        end
+        config
+      end
+
+      def http_post(url, params)
+        http url, params, :post
+      end
+
+      def http_get(url, params)
+        http url, params, :get
+      end
+
+      def http(url, params, type = :post)
+        headers = {
+          'Access' => 'application/x.atom+xml, application/xml, text/xml, */*',
+          'X-WSSE' => wsse(@username, @password),
+        }
+        case type
+        when :post
+          req = ::Net::HTTP::Post.new(url.path, headers)
+          req.form_data = params
+        when :get
+          url.query = params.map {|k,v| "#{URI::encode(k.to_s)}=#{URI::encode(v.to_s)}" }.join('&')
+          req = ::Net::HTTP::Get.new(url.request_uri, headers)
+        else
+          raise ArgumentsError.new('type must be :post or :get')
+        end
         req.basic_auth url.user, url.password if url.user
         if @proxy
           proxy = @proxy
